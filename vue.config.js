c2fe46a03637467d77faa66878c4823bf1bd44da
Pachelbel <570641740@qq.com>
2019-10-28 08:51:43 +0800

    '同一风格配置.editorconfig'

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+modules.export = {
+    configWebpack:{
+        resovle:{
+            alias: {
+                'assets':'@/assets',
+                'common':'@/common',
+                'components':'@/components',
+                'network':'@/network',
+                'views':'@/views',
+
+            }
+        }
+    }
+}
